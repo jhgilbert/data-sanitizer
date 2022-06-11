016a7ae9c7ecf4b2d8e9197fade123a798b58e2b
Jennifer Gilbert <j.h.gilbert@gmail.com>
2022-06-10 22:28:02 -0500

    Add test for shallow array

--- tests/shallowArray.test.js
+++ tests/shallowArray.test.js
@@ -1,10 +1,10 @@
 const { sanitizeFields } = require("..")
 
-test('sanitizes a shallow object', () => {
-  let data = {
-    id: 1,
-    name: 'Jen Gilbert'
-  }
+test('sanitizes a shallow array', () => {
+  let data = [
+    { id: 1, name: 'Jen Gilbert' },
+    { id: 2, name: 'Derek Reeve' }
+  ]
 
   const sanitationResult = sanitizeFields({ data, fieldNames: ['id'] })
   expect(sanitationResult).toMatchSnapshot()
